4a05e4e1a0aff30aec4d8c9a47ee55e877ab11ac
AlexandrLogunov97 <alexandr.logunov97@gmail.com>
2018-10-04 15:27:39 +0300

    note view change

--- src/components/Note.js
+++ src/components/Note.js
@@ -8,6 +8,9 @@ export class Note extends Component {
       <div>
         <h4>Tags</h4><TagList tags={this.props.note.tags}></TagList><br/>
         <br/>
+        <br/>
+        <br/>
+        <br/>
         <hr/>
         <h2>{this.props.note.title}</h2>
         <h3>{compiler(this.props.note.entry )}</h3>

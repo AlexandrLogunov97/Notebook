739633b1241be737fdc6a620c99b8d065d7b5b10
AlexandrLogunov97 <alexandr.logunov97@gmail.com>
2018-10-04 15:35:41 +0300

    final 2

--- src/components/Note.js
+++ src/components/Note.js
@@ -4,16 +4,24 @@ import { TagList } from './TagList';
 
 export class Note extends Component {
   render() {
+    let note=this.props.note?{
+      title: this.props.note.title,
+      entry: compiler(this.props.note.entry ),
+      tags: <TagList tags={this.props.note.tags}></TagList>
+    }: {
+      title: 'Empty',
+      entry: ''
+    }
     return (
       <div>
-        <h4>Tags</h4><TagList tags={this.props.note.tags}></TagList><br/>
+        <h4>Tags</h4>{note.tags}<br/>
         <br/>
         <br/>
         <br/>
         <br/>
         <hr/>
-        <h2>{this.props.note.title}</h2>
-        <h3>{compiler(this.props.note.entry )}</h3>
+        <h2>{note.title}</h2>
+        <h3>{note.entry}</h3>
       </div>
     );
   }
